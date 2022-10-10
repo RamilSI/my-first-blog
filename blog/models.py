6cc8b3b665d3c233838c64c983616878c9ce3cc5
RamilSI <rml701@mail.ru>
2022-10-10 15:51:51 +0300

    Added views to create/edit blog post inside the site.

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,11 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = models.TextField(blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    is_published = models.BooleanField(default=False)
+    # foto = models.ImageField(upload_to='pfotos/%Y/%m/%d/', blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
