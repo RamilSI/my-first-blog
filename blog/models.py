327b728c842e7335cf1ded078d15f8ed27b0dc69
RamilSI <rml701@mail.ru>
2022-10-19 23:51:40 +0300

    correct tests

--- blog/models.py
+++ blog/models.py
@@ -4,13 +4,17 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     text = models.TextField(blank=True)
+    summary = models.CharField(max_length=200)
+    category = models.CharField(max_length=255, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     is_published = models.BooleanField(default=False)
-    foto = models.ImageField(upload_to='pfotos/%Y/%m/%d/', blank=True)
+    #foto = models.ImageField(upload_to='pfotos/%Y/%m/%d/', blank=True)#TODO
+    #slug = models.SlugField(...) # todo
+
 
     def publish(self):
         self.published_date = timezone.now()
--- blog/tests.py
+++ blog/tests.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 from django.urls import resolve
 from django.test import TestCase
 from django.test import Client
 from django.http import HttpRequest
-from .models import Post
+from blog.models import Post
 from .views import *
 
 
@@ -24,3 +26,41 @@ class HomePageTest(TestCase):
         self.assertIn('<title>ТОНАЛЬ</title>', html)
         self.assertIn('<h1>ЗАЩИТНЫЕ ПОКРЫТИЯ ТОНАЛЬ</h1>', html)
         self.assertTrue(html.endswith('</html>'))
+
+
+class PostModelTest(TestCase):
+    def test_post_open_and_save(self):
+        # создай статью 1
+        # сохрани статью 1 в базе
+        post1 = Post(
+            title='post 1',
+            text='full_text 1',
+            summary='summary 1',
+            category = 'category1',
+            created_date=datetime.now())
+        post1.save()
+
+        # создай статью 2
+        # сохрани статью 2 в базе
+        post2 = Post(
+            title='post 2',
+            text='full_text 2',
+            summary='summary 2',
+            category='category2',
+            created_date=datetime.now())
+        post2.save()
+
+        # загрузи из базы все статьи
+        all_post = Post.objects.all()
+        # статей должно быть две
+        self.assertEqual(len(all_post),2)
+        # проверь: 1 загруженная статья == статья 1
+        self.assertEqual(
+            all_post[0].title,
+            post1.title)
+        # проверь: 2 загруженная статья == статья 2
+        self.assertEqual(
+            all_post[1].title,
+            post2.title)
+
+# есть новые статьи в админке, но они могут быть не опубликованы
